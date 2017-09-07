89abeda8c564df056ca4f5d1a7d1d0ed955a7d96
Evan Rubin <evan@rubin.co.za>
2017-09-07 23:02:38 +0200

    added initial test to see if server is working

--- test/server_tests.js
+++ test/server_tests.js
@@ -9,3 +9,17 @@ chai.expect();
 chai.use(chaiHttp);
 var io = require('socket.io');//(http);
 var socket = io();
+
+describe('Open server', () => {
+    it('Should return status code 200 ok when server opens', (done) => {
+      chai.request(app)
+          .get('/')
+          .end((err, res) => {
+              res.should.have.status(200);
+              chai.assert.equal(err, null);
+            done();
+          });
+    });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+});
