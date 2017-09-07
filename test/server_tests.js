77a36109216f51d0964a4a2f2ff2573cd8fcda6d
Evan Rubin <evan@rubin.co.za>
2017-09-07 23:04:39 +0200

    Testing page loading implemented

--- test/server_tests.js
+++ test/server_tests.js
@@ -21,5 +21,45 @@ describe('Open server', () => {
           });
     });
 
+    it('Should return status code 200 ok when addContacts page opens', (done) => {
+      chai.request(app)
+          .get('/addContacts')
+          .end((err, res) => {
+              res.should.have.status(200);
+              chai.assert.equal(err, null);
+            done();
+          });
+    });
+
+    it('Should return status code 200 ok when viewContacts page opens', (done) => {
+      chai.request(app)
+          .get('/viewContacts')
+          .end((err, res) => {
+              res.should.have.status(200);
+              chai.assert.equal(err, null);
+            done();
+          });
+    });
+
+    it('Should return status code 200 ok when editContacts page opens', (done) => {
+      chai.request(app)
+          .get('/editContacts')
+          .end((err, res) => {
+              res.should.have.status(200);
+              chai.assert.equal(err, null);
+            done();
+          });
+    });
+
+    it('Should return status code 404 when a random page is requested',
+    function testPath(done) {
+      chai.request(app)
+      .get('/foo')
+      .end((err, res) => {
+        res.should.have.status(404);
+        chai.expect(err).to.not.equal(null);
+        done();
+      });
+   });
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 });
