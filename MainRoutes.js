0b3e99fd9f96bcbb480dddeb7f45b2bc0a85d6d1
Evan Rubin <evan@rubin.co.za>
2017-09-07 18:47:58 +0200

    Added in routing to fetch html pages

---
+++ MainRoutes.js
@@ -0,0 +1,15 @@
+/*
+This is the javascript file used to route the html pages to eachother
+*/
+
+var express = require('express');
+var mainRouter = express.Router();
+var path = require('path');
+
+//The function used to get the html page when running the server:
+mainRouter.get('/', function(req, res){
+  res.sendfile('views/index.html');
+  res.status(200);
+});
+
+module.exports = mainRouter;
